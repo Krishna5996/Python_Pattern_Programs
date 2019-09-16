85b60b2a15d1c46701c1687427d5889a91d1c632
Krishna <krishnajadhav5996@gmail.com>
2019-09-16 18:58:17 +0530

    This is 3D diagram using turtle librarry

---
+++ turtl.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  6 08:25:11 2019
+
+@author: user
+"""
+
+from turtle import *
+from random import randint
+bgcolor('black')
+x=1
+speed(0)
+while x<400:
+    r=randint(0,255)
+    g=randint(0,255)
+    b=randint(0,255)
+    
+    colormode(255)
+    pencolor(r,g,b)
+    fd(50+x)
+    rt(90.991)
+    x+=1
+
+exitonclick()
